cdbefbc45df46243f55d976a4763d2f8595c0691
nepCoder <khanalbikash55@gmail.com>
2021-05-29 15:57:43 +0545

    resized Frame from 750*750 to 500*500

--- myCalculator/Frame.java
+++ myCalculator/Frame.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Frame extends JFrame {
     public Frame(String title){
         setTitle(title);
-        setSize(750, 750);  //tried to set size half of the screenDimension. But it doesn't support. Don't know why.
+        setSize(500, 500);  //tried to set size half of the screenDimension. But it doesn't support. Don't know why.
         
 
     }
