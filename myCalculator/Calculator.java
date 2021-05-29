40ae496661ff79abec026ff3741e888d67f40dcf
nepCoder <khanalbikash55@gmail.com>
2021-05-29 15:53:18 +0545

    display the frame on screen

--- myCalculator/Calculator.java
+++ myCalculator/Calculator.java
@@ -1,5 +1,12 @@
 package myCalculator;
 
+import javax.swing.JFrame;
+
 public class Calculator {
-    
+    public static void main(String[] args) {
+        Frame f = new Frame("myFrame");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationByPlatform(true);
+        f.setVisible(true);  
+    }
 }
--- myCalculator/Frame.java
+++ myCalculator/Frame.java
@@ -1,5 +1,12 @@
 package myCalculator;
 
-public class Frame {
-    
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+    public Frame(String title){
+        setTitle(title);
+        setSize(750, 750);  //tried to set size half of the screenDimension. But it doesn't support. Don't know why.
+        
+
+    }
 }
